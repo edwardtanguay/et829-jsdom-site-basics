f47093f8b60d62b77c7b6d9dba181c741e745962
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-02 10:57:38 +0100

    site has styling

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,6 @@
 export const Section = (title, text) => {
-	return `
-  <h2 class="text-orange-600">>>> ${title}</h2>
-  <p class="ml-4">${text}</p>
+	return ` 
+<h2 class="text-orange-600">>>> ${title}</h2>
+<p class="ml-4">${text}</p>
 	`;
-}
+};
