64e7665e3f46ea802fe5d3524f225a684b0ae7b1
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-02 11:22:30 +0100

    showing todos with innerHTML

--- src/main.js
+++ src/main.js
@@ -21,13 +21,18 @@ const todos = [
   }
 ];
 
-document.querySelector('#app').innerHTML = `
+document.querySelector('#app').innerHTML = /*html*/ `
 <main>
   <h1 class="text-2xl text-blue-800 mb-4">${title}</h1>
   ${Section("Intro", "Welcome to this site.")}
   ${Section("More Info", "This website is going to be a todo list.")}
   ${Section("Third Section", "More information about the todo list.")}
-
-
+  
+  <h2 class="mt-6 text-2xl mb-4">Todos</h2>
+  ${todos.map(todo => {
+    return `
+    <div class="bg-slate-400 mb-2 p-2 rounded">${todo.text} - ${todo.finished ? 'FINISHED' : '(doing...)'}</div>
+    `
+  }).join('')}
 </main>
 `;
