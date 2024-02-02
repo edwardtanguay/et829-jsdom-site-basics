744108d61c71e9115affbd2abfe297cb34c539b7
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-02 10:38:59 +0100

    tailwind in

--- components/Section.js
+++
@@ -1,6 +0,0 @@
-export const Section = (title, text) => {
-	return `
-  <h2>>>> ${title}</h2>
-  <p>${text}</p>
-	`;
-}
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const title = 'Our Todo List';
 
 document.querySelector('#app').innerHTML = `
 <main>
-  <h1>${title}</h1>
+  <h1 class="text-2xl text-blue-800 mb-4">${title}</h1>
   ${Section("Intro", "Welcome to this site.")}
   ${Section("More Info", "This website is going to be a todo list.")}
   ${Section("Third Section", "More information about the todo list.")}
